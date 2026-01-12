d00cae9bbf48d63f8e4fa135aef43cad6325fbf5
chibz.dee <didiguchibuikem@gmail.com>
2026-01-12 17:39:50 -0500

    commit

--- SwitchStatements.java
+++ SwitchStatements.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 
 public class SwitchStatements {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner keyboard = new Scanner(System.in);
 

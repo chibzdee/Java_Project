575b23ced99c2363d096554b6e33d1d47afbee9c
chibz.dee <didiguchibuikem@gmail.com>
2026-01-12 17:01:40 -0500

    commit this change

--- LogicalOperators.java
+++ LogicalOperators.java
@@ -1,13 +1,25 @@
 public class LogicalOperators {
     public static void main(String[] args){
-        boolean isColdOutside = true;
-        boolean haveWorkToday = false;
-        boolean isWeekend = true;
+//        boolean isColdOutside = true;
+//        boolean haveWorkToday = false;
+//        boolean isWeekend = true;
+//
+//        if (isColdOutside && haveWorkToday && isWeekend){
+//            System.out.println("Go to work today!! ");
+//        } else{
+//            System.out.println("No work today");
+//        }
 
-        if (isColdOutside && haveWorkToday && isWeekend){
-            System.out.println("Go to work today!! ");
-        } else{
-            System.out.println("No work today");
+        // real life example
+
+        boolean isLoggedIn = true;
+        boolean isAdmin = false;
+        int securityLevel = 3; // where 1 is the highest
+
+        if (isLoggedIn && (isAdmin || securityLevel <= 2)) {
+            System.out.println("Access Granted");
+        } else {
+            System.out.println("Access Denied");
         }
     }
 }
